0b97e4c29f6c4f3842910cafe1cfef6769e8e995
Pedro Cruz <pedro@localhost.localdomain>
2018-01-18 20:30:25 -0200

    atualizando elseif

---
+++ if_elseif_01.php
@@ -0,0 +1,12 @@
+<?php
+/**Testando IF e ELSEIF no PHP
+ * By Pedro Corin */
+$a = 10;
+
+if ($a == 1){
+    echo"$a vale 1";
+}elseif ($a == 2){
+    echo"$a vale 2";
+}elseif($a == 10){
+    echo"$a vale 10";
+}?>
